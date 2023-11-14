b0ed5170f6d9a8cc9ad8e10e2960dfb8fe7201a6
jaimesilvalorca <jaimesilvalorca@gmail.com>
2023-11-14 17:42:14 -0300

    Styles profile update

---
+++ src/Presentation/views/profile/update/Styles.tsx
@@ -0,0 +1,60 @@
+import { StyleSheet } from "react-native";
+
+
+const ProfileUpdateStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'white',
+    },
+    imageBackground: {
+      width: '100%',
+      height: '100%'
+    },
+    form: {
+      backgroundColor: 'white',
+      width: '100%',
+      height: '50%',
+      position: 'absolute',
+      bottom: 0,
+      borderTopLeftRadius: 40,
+      borderTopRightRadius: 40,
+      padding: 20
+    },
+    logoContainer: {
+      position: 'absolute',
+      alignSelf: 'center',
+      top: '10%'
+    },
+    logoImage: {
+      alignSelf: 'center',
+      width: 100,
+      height: 100,
+  
+    },
+    logoText: {
+      color: 'white',
+      fontSize: 20,
+      marginTop: 10,
+      fontFamily: '',
+      textAlign: 'center',
+      fontWeight: 'bold'
+  
+    },
+    formText: {
+      fontWeight: 'bold',
+      textAlign: 'center',
+      fontSize: 20
+    },
+    loading:{
+      position:'absolute',
+      bottom:0,
+      top:0,
+      right:0,
+      left:0,
+      
+
+
+    }
+  });
+
+  export default ProfileUpdateStyles
